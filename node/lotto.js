aa02804aab162ba67f89127b5f1163772c38e7b4
bryce0516 <jayhspark@gmail.com>
2021-03-13 16:19:59 +0900

    finish lotto

--- node/lotto.js
+++ node/lotto.js
@@ -1,15 +1,26 @@
 const readline = require('readline')
 var fs = require('fs');
+const parse = require('csv-parse/lib/sync');
+
 const rl = readline.createInterface({
   input: process.stdin,
   outpit: process.stdout
 })
 
-let parser = new DOMParser();
+function compareNumbers(a, b) {
+  return a - b;
+}
+const csv = fs.readFileSync('./lotto.csv');
+const records = parse(csv.toString('utf-8'));
+let toparse = records.map(el => el.map(el => parseInt(el)))
+const final = toparse.map(el => el.sort(compareNumbers))
 
-let pickedNum = []
+// console.log(JSON.stringify(final[0]) === JSON.stringify([7,9,22,27,37,42]))
+
+
+let result = []
 const pick = () => {
-
+  let pickedNum = []
   while(pickedNum.length < 6){
     let random = Math.random() * 45
     let filter = Math.floor(random) + 1
@@ -18,12 +29,17 @@ const pick = () => {
       pickedNum.push(filter)
     }
   }
-  const stringContainingHTMLSource = fs.readFile('excel.xls', 'utf8', function (err, data){
-    doc = parser.parseFromString(data, "text/html")
-  })
-
-  console.log(pickedNum)
+  result = pickedNum.sort(compareNumbers)
 }
-
+const start = final.find(el => 
+  JSON.stringify(el) === JSON.stringify(result)
+)
 
 pick()
+
+if (start) {
+  console.log('fail the lotto')
+} else {
+  console.log(result)
+}
+
