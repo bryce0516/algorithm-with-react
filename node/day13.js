2ffae0bbc21f668dfa66f0a72066d78848d54c2c
bryce0516 <jayhspark@gmail.com>
2021-04-08 21:59:40 +0900

    해시테이블을 이용한 계산 추가

--- node/day13.js
+++ node/day13.js
@@ -9,3 +9,19 @@ function findSum(arr, weight) {
   }
   console.log(-1);
 }
+
+function findSumBetter(arr, weight) {
+  var hashtable = {};
+  for (var i = 0, arrLength = arr.length; i < arrLength; i++) {
+    var currentElement = arr[i];
+    diffrence = weight - currentElement;
+    if (hashtable[currentElement] != undefined) {
+      return [i, hashtable[currentElement]];
+    } else {
+      hashtable[diffrence] = i;
+    }
+  }
+  return -1;
+}
+
+console.log(findSumBetter([1, 2, 3, 4, 5], 9));
