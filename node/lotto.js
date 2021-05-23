0a8554fed13fad4ff93d068bfa4ffd3cbf4bbc19
bryce0516 <jayhspark@gmail.com>
2021-05-23 18:21:17 +0900

    수정

--- node/lotto.js
+++ node/lotto.js
@@ -12,15 +12,47 @@ function compareNumbers(a, b) {
 }
 const lottocsv = fs.readFileSync("./lotto.csv");
 const lotto2csv = fs.readFileSync("./lotto2.csv");
+const lotto3csv = fs.readFileSync("./lotto3.csv");
 const records = parse(lottocsv.toString("utf-8"));
 const records2 = parse(lotto2csv.toString("utf-8"));
+const records3 = parse(lotto3csv.toString("utf-8"));
 let toparse = records.map((el) => el.map((el) => parseInt(el)));
 let toparse2 = records2.map((el) => el.map((el) => parseInt(el)));
+let toparse3 = records3.map((el) => el.map((el) => parseInt(el)));
 const final = toparse.map((el) => el.sort(compareNumbers));
 const final2 = toparse2.map((el) => el.sort(compareNumbers));
-// console.log(JSON.stringify(final[0]) === JSON.stringify([7,9,22,27,37,42]))
+const final3 = toparse3.map((el) => el.sort(compareNumbers));
+// console.log("this is lotto final3", final3.length);
+const thisWeek = [6, 21, 36, 38, 39, 43];
+let pushedArr = [];
+let i = 0;
 
-const lottoMaker = (final, result = []) => {
+// final3[0].map((el) => {
+//   thisWeek.map((el2, index) => {
+//     if (el === el2) {
+//       pushedArr.push(el2);
+//     }
+//   });
+// });
+// console.log("what inside ", pushedArr);
+
+checkLottoNumber = (result1, result2, pushedArr = [], i) => {
+  if (i > 49) return console.log("50이큼", i);
+
+  result1[i].map((el) => {
+    result2.map((el2, index) => {
+      if (el === el2) {
+        pushedArr.push(el2);
+      }
+    });
+  });
+  console.log("what inside ", pushedArr, i);
+  checkLottoNumber(final3, thisWeek, (pushedArr = []), i + 1);
+};
+
+checkLottoNumber(final3, thisWeek, pushedArr, 0);
+
+const lottoMaker = (final, final2, result = []) => {
   let pickedNum = [];
   let i = 0;
   const sixNumbers = (i) => {
@@ -42,6 +74,7 @@ const lottoMaker = (final, result = []) => {
     (el) => JSON.stringify(el) === JSON.stringify(sixNumbersSort)
   );
   let arr;
+
   if (compareSix2) return console.log("말도안되는 숫자조합에 걸림");
   if (compareSix) return console.log("진짜 6개나와서 다시해야함");
 
@@ -68,22 +101,14 @@ const lottoMaker = (final, result = []) => {
   if (res) {
     console.log("allpass number", sixNumbersSort, final.length);
   } else {
-    lottoMaker(final);
+    lottoMaker(final, final2);
   }
-
-  // let compareFive = result2.map((el) =>
-  //   el.map((el2, index) => {
-  //     console.log("el2 index", el2, result1[index], el2 === result1[index]);
-  //     if (el2 === result1[index]) {
-
-  //     }
-  //   })
-  // );
-  // console.log("this is arr", arr);
 };
-for (var i = 0; i < 2; i++) {
-  lottoMaker(final);
-}
+
+// for (var i = 0; i < 50; i++) {
+//   lottoMaker(final, final2);
+// }
+
 // lottoMaker(final);
 // const pick = () => {
 //   let pickedNum = [];
